a8f9fdfb5e9fbfe249f96ceec8206d0c8f16f287
Kim Dong Hwan <sioil3333.gmail.com>
2023-01-28 19:00:41 +0900

    solve chapter5

---
+++ practiceDoitDataStructure/5장/5-8.py
@@ -0,0 +1,24 @@
+# 행과 열에 퀸을 1개 배치하는 조합을 재귀적으로 나욜하기
+
+pos = [0] * 8
+flag = [False] * 8
+
+def put() -> None:
+
+    for i in range(8):
+        print(f'{pos[i]:2}', end=' ')
+    print()
+
+def set(i: int) -> None:
+
+    for j in range(8):
+        if not flag[j]:
+            pos[i] = j
+            if i == 7:
+                put()
+            else:
+                flag[j] = True
+                set(i + 1)
+                flag[j] = False
+
+set(0)
